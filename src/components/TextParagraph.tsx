0787f1df49ee2d71568f14fc6eb3a54a99e6a554
saurluca <spuckteller@gmail.com>
2024-11-06 00:15:25 +0100

    deleted unnesarcy files

--- src/components/TextParagraph.tsx
+++ src/components/TextParagraph.tsx
@@ -7,7 +7,7 @@ interface TextParagraphProps {
 
 const TextParagraph: FC<TextParagraphProps> = ({ children, alignment }) => {
     return (
-        <div className={`text-${alignment} `}>
+        <div className={alignment === 'right' ? 'text-right' : 'text-left'}>
             <p className={`text-2xl text-gray-600 max-w-[600px] ${alignment === 'right' ? 'ml-auto' : ''}`}>
                 {children}
             </p>
